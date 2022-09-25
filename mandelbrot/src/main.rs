311f5eea987438055d970ba70ba3eb5d9dd0b143
jp-knj <kenji.tomita1996@gmail.com>
2022-09-26 08:53:02 +0900

    :+1: add escape time fn

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -15,3 +15,13 @@ fn square_add_loop(c: f64){
         z = z * z + c;
     }
 }
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i)
+        }
+        z = z * z + c;
+    }
+}
