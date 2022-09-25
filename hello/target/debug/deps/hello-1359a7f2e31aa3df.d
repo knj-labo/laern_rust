cf6e7a51dda7562792df324a190cab87b0ab0122
jp-knj <kenji.tomita1996@gmail.com>
2022-09-25 11:14:38 +0900

    :shirt: insert .gitignore file why: unneccery files  to stage

---
+++ hello/target/debug/deps/hello-1359a7f2e31aa3df.d
@@ -0,0 +1,5 @@
+/Users/kenji/Project/01_private/27_learn_rust/hello/target/debug/deps/hello-1359a7f2e31aa3df: src/main.rs
+
+/Users/kenji/Project/01_private/27_learn_rust/hello/target/debug/deps/hello-1359a7f2e31aa3df.d: src/main.rs
+
+src/main.rs:
