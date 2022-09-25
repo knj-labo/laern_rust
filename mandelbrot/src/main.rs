126fd9928dc879286a220c0364cd32ab42e5914e
jp-knj <kenji.tomita1996@gmail.com>
2022-09-26 08:46:29 +0900

    :+1: learn loop of rust

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn square_loop(mut x: f64) {
+    loop {
+        x = x * x;
+    }
+}
+
+use num::Complex;
+fn square_add_loop(c: f64){
+    let mut z = Complex{ re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
