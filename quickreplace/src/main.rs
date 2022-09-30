fe87a8770d3f9501d4e3ec060e038cfa03b170f8
jp-knj <kenji.tomita1996@gmail.com>
2022-09-30 23:32:25 +0900

    feat: create string of array

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[drive(Debud)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String
+}
+
+use text_colorizer::*;
+
+fn print_usage() {
+    eprintln!("{} - change occurence of one string into another",
+              "quickreplace".green());
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
