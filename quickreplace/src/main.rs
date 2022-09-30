efe762776e09e1dde13176ec2e89349518fe41b5
jp-knj <kenji.tomita1996@gmail.com>
2022-09-30 23:42:09 +0900

    :+1: commandline interface

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,4 +1,7 @@
-#[drive(Debud)]
+use text_colorizer::*;
+use std::env;
+
+#[derive(Debug)]
 struct Arguments {
     target: String,
     replacement: String,
@@ -6,7 +9,28 @@ struct Arguments {
     output: String
 }
 
-use text_colorizer::*;
+fn main() {
+    let args = parse_args();
+    println!("{:?}", args);
+}
+
+fn parse_args() -> Arguments{
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!("{} wrong number of arguments: expected 4, got {}",
+                  "Error:".red().bold(),args.len());
+        std::process::exit(1);
+    }
+
+    Arguments{
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone()
+    }
+}
 
 fn print_usage() {
     eprintln!("{} - change occurence of one string into another",
