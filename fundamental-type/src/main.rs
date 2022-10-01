1c791c1b589f050a0cde1123315157f7d087521a
jp-knj <kenji.tomita1996@gmail.com>
2022-10-02 00:12:51 +0900

    :construction: try to learn string type

--- fundamental-type/src/main.rs
+++ fundamental-type/src/main.rs
@@ -4,3 +4,15 @@ fn build_vector()->Vec<i16> {
     v.push(20);
     v
 }
+
+fn main() {
+    assert_eq!( 10_i8 as u16, 10_u16);
+    assert_eq!( 2525_u16 as i16, 2525_i16);
+    assert_eq!( -1_i16 as i32, -1_i32);       // sign-extended
+    assert_eq!( 65535_u16 as i32, 65535_i32); // zero-extended
+    assert_eq!( 1000_i16 as u8, 232_u8);
+
+    assert_eq!(2_u16.pow(4), 16);               // expatiation
+    assert_eq!((-4_i32).abs(), 4);              // absolute value
+    assert_eq!((0b101101_u8).count_one(), 4);   // population count
+}
