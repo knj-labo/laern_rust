69737dafaecc4758eb7dc5eea558eb4338412d69
jp-knj <kenji.tomita1996@gmail.com>
2022-10-02 11:06:10 +0900

    :construction: learn timing

--- own_and_move/src/main.rs
+++ own_and_move/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn print_padovan() {
+    let mut padovan = vec![1,1,1]; // 確保
+    for i in 3..10 {
+        let next = padovan[i-3] + padovan[i-2];
+        padovan.push(next);
+    }
+    println!("P(1..10) = {:?}", padovan); // 解放
+}
