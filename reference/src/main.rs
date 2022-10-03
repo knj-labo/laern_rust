719fbea27ef3c7d084698a696a27e580a0008fd3
jp-knj <kenji.tomita1996@gmail.com>
2022-10-04 08:51:40 +0900

    feat: view name of artist

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+type Table = HashMap<String, Vec<String>>;
+
 fn main() {
-    println!("Hello, world!");
+    let mut table = Table::new();
+    table.insert("Gesualdo".to_string(),
+                 vec!["many madrigals".to_string(),
+                      "Tenebrae Responsiria".to_string()]);
+    table.insert("Caravaggio".to_string(),
+                 vec!["The Musicians".to_string(),
+                      "The Calling of St.Matthew".to_string()]);
+    show(table)
+}
+
+fn show(table: Table) {
+    for(artist, works) in table {
+        println!("works by {}:", artist);
+        for work in works {
+            println!(" {}", work);
+        }
+    }
 }
