fbe02abf4556361861e51bb52cfbf4413e3e2d04
jp-knj <kenji.tomita1996@gmail.com>
2022-10-26 22:23:47 +0900

    :sparkles: strace

--- crash/src/main.rs
+++ crash/src/main.rs
@@ -3,12 +3,12 @@ async fn main() {
     let response = hyper::Client::new().get("http://example.org".parse().unwrap())
         .await
         .unwrap();
-    println!(
-        "Got HTTP {}, with headers: {:#?}",
-        response.status(),
-        response.headers()
-    );
 
-    let body = response.body();
-    println!("Body: {:?}", body);
+    let mut body = response.into_body();
+
+    // 1度に1つのバッファを取得しながらデータを polling することができる。
+    while let Some(buffer) = std::future::poll_fn(|cx| Pin::new(&mut body).poll_data(cx)).await {
+        let buffer = buffer.unwrap();
+        println!("Read {} bytes", buffer.len());
+    }
 }
