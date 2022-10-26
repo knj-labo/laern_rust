3d5a5a925a22136edb5bb09374271cb8cd3885b3
jp-knj <kenji.tomita1996@gmail.com>
2022-10-26 22:06:24 +0900

    :sparkles: learn http

--- crash/src/main.rs
+++ crash/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let response = reqwest::get("http://example.org").await.unwrap();
+    println!(
+        "Got HTTP {}, with headers: {:#?}",
+        response.status(),
+        response.headers()
+    );
 }
