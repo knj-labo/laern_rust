11242a3a57f466b2a1c4bcf624dc6253b4266186
jp-knj <kenji.tomita1996@gmail.com>
2022-10-30 15:52:05 +0900

    :sparkles: install stream for asynchronous

--- crash/src/main.rs
+++ crash/src/main.rs
@@ -1,14 +1,16 @@
+use futures::TryStreamExt;
+
 #[tokio::main]
 async fn main() {
-    let response = hyper::Client::new().get("http://example.org".parse().unwrap())
+    let response = hyper::Client::new()
+        .get("http://example.org".parse().unwrap())
         .await
         .unwrap();
 
     let mut body = response.into_body();
 
     // 1度に1つのバッファを取得しながらデータを polling することができる。
-    while let Some(buffer) = std::future::poll_fn(|cx| Pin::new(&mut body).poll_data(cx)).await {
-        let buffer = buffer.unwrap();
+    while let Some(buffer) = body.try_next().await.unwrap() {
         println!("Read {} bytes", buffer.len());
     }
 }
