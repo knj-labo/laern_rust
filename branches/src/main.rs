67c5d9b9908b4113facc89a56665e59662a92411
jp-knj <kenji.tomita1996@gmail.com>
2022-11-23 23:59:53 +0900

    :sparkles: understand refarence

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -3,13 +3,26 @@
 // &記号が参照
 fn main() {
   let s1 = String::from("hello");
-
   let len = calculate_length(&s1);
-
   // '{}'の長さは、{}です
   println!("The length of '{}' is {}.", s1, len);
+
+  change_string();
 }
 
+// 一つしか可変な参照を持てない
+// この制約がある利点は、コンパイラがコンパイル時にデータ競合を防ぐことができる点です。 データ競合とは、競合条件と類似していて、これら3つの振る舞いが起きる時に発生します:
+// - 2つ以上のポインタが同じデータに同時にアクセスする。
+// - 少なくとも一つのポインタがデータに書き込みを行っている。
+// - データへのアクセスを同期する機構が使用されていない。
+fn change_string () {
+    let mut s = String::from("hello");
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
 
 fn calculate_length(s: &String) -> usize {
     s.len()
