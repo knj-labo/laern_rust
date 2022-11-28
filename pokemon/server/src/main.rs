1bf1a163f6406282d49c1ea1f0d1be1e3350297d
jp-knj <kenji.tomita1996@gmail.com>
2022-11-29 00:11:42 +0900

    :sparkles: initial project

--- pokemon/server/src/main.rs
+++ pokemon/server/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_rt::main]
+async fn main(){
+  // HOST 0.0.0.0, PORT 4000
+  let url = "0.0.0.0:4000";
+  println!("server started on {} using actix", url);
 }
