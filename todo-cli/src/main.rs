9a1dc1e7967451e0a7095296ebab368a6fd85c4d
jp-knj <kenji.tomita1996@gmail.com>
2022-11-30 00:31:50 +0900

    :sparkles: wip

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -3,6 +3,18 @@ fn main() {
     let item = std::env::args().nth(2).expect("Please specify an item");
 
     println!("{:?}, {:?}", action, item);
+
+    let mut todo = Todo {
+        map:HashMap::new(),
+    };
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
 }
 
 use std::collections::HashMap;
@@ -17,5 +29,17 @@ impl Todo {
         // insert a new item into our map.
         // we pass true as value
         self.map.insert(key, true);
+    }
+    // -> annotates the returned type from the function. returning a Result.
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        // iterate over the map, and format each string, separating key and value with a tab character and each line with a new line.
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            // push the formatted string into a content variable.
+            content.push_str(&record)
+        }
+        // write content inside a file called db.txt.
+        std::fs::write("db.txt", content)
     }
 }
